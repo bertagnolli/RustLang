b0dcd20de3d4a689337c249d639e80531172d82a
bertagnolli <carlosb@live.com>
2022-06-13 16:06:54 +0100

    Slice example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let mut num = 5;
-    num = 3;
-    println!("{}", num);
+    let arr = [0, 1, 2, 3]; // Known length
+    let slice = &arr[1 .. 3]; // A slice is a sement of an array. Syntax: let sliceName = &slicedArray[index .. <indexN-1>]; Last index isn't inclusive
+    // array above will return arr[1 to 2], 3 is not inclusive
+    borrowing_slice(arr, slice);
+}
+
+fn borrowing_slice(arr: [u8; 4], slice: &[u8]) {
+    println!("{:?}", arr);
+    println!("{:?}", slice);
+    println!("length: {}", slice.len());
+    println!("{} {}", slice[0], slice[1]);
 }
