1d046bc4398104dacd85d027c5ec5cf01b279c74
bertagnolli <carlosb@live.com>
2022-06-13 17:17:11 +0100

    If-else example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,20 +1,13 @@
 fn main() {
-    // String below is type string Literal, stored in Stack memory and immutable
-    let mut stringLiteral = "hello world";
+    
+    let n = 3;
+    if n > 0 {
+        println!("greater than zero");
+    } else if n < 0 {
+        println!("smaller than zero");
+    } else {
+        println!("is zero");
+    }
 
-    // this string below is of type String and will be stored in heap memory. This String type can be altered if 
-    // it is set as 'mut'
-    let mut string = String::from("Hello World");
-
-    let slice = &string[.. 6];
-
-    println!("{}", slice);
-    println!("{}", &string);
-
-    string.push('a'); // pushes character a to string
-    string.push_str(" add string"); // adds " add string" to string
-    println!("{}", string); 
-    string = string.replace("add", "replace"); // replaces the word "add" with word "replace"
-    println!("{}", string); 
 }
 
