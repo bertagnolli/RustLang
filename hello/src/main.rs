1fdfcb25b951f852d7e7f0b9fb47c66c8c7a7324
bertagnolli <carlosb@live.com>
2022-06-13 17:41:06 +0100

    While loop example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    for i in 0..6 {
+    let mut i = 0;
+    while i< 4 {
         println!("{}", i);
+        i += 1;
+
+        if i == 3 {
+            println!("exit");
+            break
+        }
     }
 
 }
