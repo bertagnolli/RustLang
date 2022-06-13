7fc1fc0c536b159ca7264188b402db8b12dc6e60
bertagnolli <carlosb@live.com>
2022-06-13 17:39:20 +0100

    For loop example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,12 +1,6 @@
 fn main() {
-    
-    let n = 3;
-    if n > 0 {
-        println!("greater than zero");
-    } else if n < 0 {
-        println!("smaller than zero");
-    } else {
-        println!("is zero");
+    for i in 0..6 {
+        println!("{}", i);
     }
 
 }
