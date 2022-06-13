aeb1070b5f07beb3608f2108d46133ddc622979e
bertagnolli <carlosb@live.com>
2022-06-13 12:09:33 +0100

    Functions example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,16 +1,8 @@
 fn main() {
-    // Tuples can hold different types of data
-    let tupleEx: (u8, bool, f32) = (5, true, 2.1);
-
-    // Print each element in a tuple, syntax is tupleName.index
-    println!("first {}, second {}, third {}", tupleEx.0, tupleEx.1, tupleEx.2);
-
-    // Print all elements inside a tuple, same as array {:?:}    
-    println!("{:?}", tupleEx);
-
-    // Descontruct a tuple
-    let (a, b, c) = tupleEx;
-    println!("Disconstructed tuple {} {} {}", a, b, c);
-
+    println!("{}", is_even(2));
 }
 
+pub fn is_even(num: u8) -> bool {
+    let digit: u8 = num % 2;
+    digit == 0
+}
